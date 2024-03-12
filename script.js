d8f295f16d0b21c7cf0d5c2fbff73fab2c4c5323
Jay-A-Kad <jaykad982@gmail.com>
2024-03-12 19:25:22 +0530

    added snake move logic and game start mod

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 const board = document.getElementById('game-board');
 
 // game variables
-const gridSize = 20
-let snake = [{ x: 10, y: 10 }];
+const gridSize = 20;
+let snake = [{ x: 10, y: 10 },];
 let food = generateFood();
+let direction = 'right';
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted = false;
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
 
 
 // draw game map, snake and food
@@ -48,9 +54,63 @@ function drawFood() {
 
 draw();
 
+// generate food
+
 function generateFood() {
     const x = Math.floor(Math.random() * gridSize + 1);
     const y = Math.floor(Math.random() * gridSize + 1);
     return { x, y };
 }
 
+
+// moving a snake
+function move() {
+    const head = { ...snake[0] };
+    switch (direction) {
+        case 'right':
+            head.x++;
+            break;
+        case 'up':
+            head.y--;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+    }
+    snake.unshift(head);
+
+    if (head.x === food.x && head.y === food.y) {
+        food = generateFood();
+        clearInterval();
+        gameInterval = setInterval(() => {
+            move();
+            draw();
+
+        }, gameSpeedDelay);
+    } else {
+        snake.pop();
+    }
+}
+
+// start game 
+function startGame() {
+    gameStarted = true;   //keep track of game 
+    instructionText.style.display = 'none';
+    logo.style.display = 'none';
+    gameInterval = setInterval(() => {
+        move();
+        checkCollision();
+        draw();
+    }, gameSpeedDelay);
+}
+
+
+
+
+// setInterval(() => {
+//     move();
+//     draw();
+// }, 200);
